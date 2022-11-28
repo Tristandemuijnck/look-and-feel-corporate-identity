ebdd23f9f69244e81813af1df8e0cc454b4f52e4
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-11-28 22:56:56 +0100

    v1.3
    Mobile menu toggle

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    var menu = document.getElementById("primaire-nav");
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    } else{
+        menu.style.display = "block";
+    }
+}
