8e9825de572da3c3220443cf71629d40c10c9984
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-11-29 23:47:16 +0100

    v1.5 Checklist page mobile CSS
    -Added checklist page mobile css

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,30 @@ function toggleMenu(){
         menu.style.display = "block";
     }
 }
+
+function switchDisplay(id, buttonId){
+    let elementId = document.getElementById(id);
+    let button = document.getElementById(buttonId);
+    
+    if(elementId.style.display == 'none'){
+        elementId.style.setProperty('display', 'flex');
+        button.innerHTML = "Minder info";
+    }
+    else if(elementId.style.display == 'flex'){
+        elementId.style.setProperty('display', 'none');
+        button.innerHTML = "Meer info";
+    }
+}
+
+function switchButtonText(classDisplayChild){
+    const infoButton = document.querySelectorAll('.rl-card-btn');
+
+    infoButton.forEach((infoButtonChild) => {
+        if(classDisplayChild == 'none'){
+            infoButtonChild.innerHTML = "Meer info";
+        }
+        else if(classDisplayChild == 'flex'){
+            infoButtonChild.innerHTML = "Minder info";
+        }
+    });
+}
