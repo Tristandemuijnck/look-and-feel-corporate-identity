313e3ff26b6674a7a2cdd8b068cbe680dd9d6622
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-12-08 23:20:44 +0100

    V2.6 Checkbox counting + JS cleanup
    - Added counter for checkboxes per category
    - Removed JS from HTML
    - Removed CSS from JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,3 @@
-// function toggleMenu(){
-//     var menu = document.getElementById("primaire-nav");
-//     if(menu.style.display == "block"){
-//         menu.style.display = "none";
-//     } else{
-//         menu.style.display = "block";
-//     }
-// }
-
 // Navigatie menu openen
 let menuBtn = document.getElementById('menu-btn');
 let primaireNav = document.getElementById('primaire-nav');
@@ -15,60 +6,77 @@ menuBtn.addEventListener('click', () => {
     primaireNav.classList.toggle("show");
 });
 
-// Checboxes tellen
+// Checkboxes tellen
+var waarneembaarCheckboxes = document.querySelectorAll('#rl-waarneembaar input[type=checkbox]');
+var robuustCheckboxes = document.querySelectorAll('#rl-robuust input[type=checkbox]');
+var begrijpelijkCheckboxes = document.querySelectorAll('#rl-begrijpelijk input[type=checkbox]');
+var bedienbaarCheckboxes = document.querySelectorAll('#rl-bedienbaar input[type=checkbox]');
 
-// Waarneember section
-let waarneembaarCheckboxes = document.getElementsByName('richtlijn-cb-waarneembaar');
-let numberOfCheckboxesWaarneembaar = 0;
-
-console.log(waarneembaarCheckboxes);
-
-for (let i = 0; i < waarneembaarCheckboxes.length; i++) {
-    waarneembaarCheckboxes[i].addEventListener('change', countChecks(waarneembaarCheckboxes[i])); 
-    console.log(waarneembaarCheckboxes[i]);
-}
-
-
-function countChecks(checkbox) {
-    if (checkbox.checked){
-        numberOfCheckboxesWaarneembaar++;
-    }
-
-    console.log(numberOfCheckboxesWaarneembaar);
-}
- 
-
-// Robuust section
-
-// Begrijpelijk section
-
-// Bedienbaar section
-
-
-// Card openen
-function switchDisplay(id, buttonId){
-    let elementId = document.getElementById(id);
-    let button = document.getElementById(buttonId);
-    
-    if(elementId.style.display == 'none'){
-        elementId.style.setProperty('display', 'flex');
-        button.innerHTML = "Minder info";
-    }
-    else if(elementId.style.display == 'flex'){
-        elementId.style.setProperty('display', 'none');
-        button.innerHTML = "Meer info";
-    }
-}
-
-function switchButtonText(classDisplayChild){
-    const infoButton = document.querySelectorAll('.rl-card-btn');
-
-    infoButton.forEach((infoButtonChild) => {
-        if(classDisplayChild == 'none'){
-            infoButtonChild.innerHTML = "Meer info";
-        }
-        else if(classDisplayChild == 'flex'){
-            infoButtonChild.innerHTML = "Minder info";
-        }
+waarneembaarCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        countChecks(checkbox, this.closest(".rl-cards").parentElement.id);
     });
+});
+
+robuustCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        countChecks(checkbox, this.closest(".rl-cards").parentElement.id);
+    });
+});
+
+begrijpelijkCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        countChecks(checkbox, this.closest(".rl-cards").parentElement.id);
+    });
+});
+
+bedienbaarCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        countChecks(checkbox, this.closest(".rl-cards").parentElement.id);
+    });
+});
+
+function countChecks(checkbox, categorie) {
+    var progress = checkbox.closest(".rl-cards").querySelectorAll('input[type=checkbox]:checked').length;
+
+    switch (categorie) {
+        case "rl-waarneembaar":
+            document.getElementById("waarneembaar-count").innerHTML = progress;
+            break;
+
+        case "rl-robuust":
+            document.getElementById("robuust-count").innerHTML = progress;
+            break;
+
+        case "rl-begrijpelijk":
+            document.getElementById("begrijpelijk-count").innerHTML = progress;
+            break;
+        
+        case "rl-bedienbaar":
+            document.getElementById("bedienbaar-count").innerHTML = progress;
+            break;
+    
+        default:
+            break;
+    }
 }
+
+// Card openen alternatief
+let cardButtons = document.querySelectorAll('.rl-card-btn');
+let cardsContent = document.querySelectorAll('.rl-card-content');
+
+cardButtons.forEach((cardButton) => {
+    cardButton.addEventListener('click', function () {
+        let parentElement = this.closest('.rl-card-content-wrapper');
+        let targetElement = parentElement.querySelector('.rl-card-extra');
+        let btnElement = parentElement.querySelector('.rl-card-btn');
+        
+        targetElement.classList.toggle("showCard");
+        
+        if(targetElement.classList.contains("showCard")){
+            btnElement.innerHTML = "Minder info";
+        } else {
+            btnElement.innerHTML = "Meer info";
+        }
+    })
+})
